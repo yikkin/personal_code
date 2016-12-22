1d35c2996db8206ca5c2b03c58de8b53bd51fdb0
yikkin <yongwe52@gmail.com>
2016-12-23 00:10:37 +0100

    Add files via upload
    #recommadation system for movies rated by viewers

---
+++ machine_learning_home/recommandation_systems.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 15:42:00 2016
+
+@author: yjl20
+"""
+
+#recommender systems:
+    #collaborative = predict what you like based on what others users have liked
+    #content-based = predict what you like in function f what you liked in the past
+    
+#dependencies
+#pip install lightm
+
+import numpy as np
+import scipy as sc
+import lightm as lg
