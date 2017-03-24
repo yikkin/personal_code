82b5b50032fda4d1bc571719b1ab25dfdf0b2d9b
Yongwe Jean-Luc <yongwe52@gmail.com>
2017-03-25 00:45:01 +0100

    Update gender_classification_tree.py

--- machine_learning_home/random_classification/gender_classification_tree.py
+++ machine_learning_home/random_classification/gender_classification_tree.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 23 00:20:12 2016
-
-@author: yjl20
-"""
 #method importation
 from sklearn import tree
 import random as rnd
+import pandas as pd
 
 #creation of data
 nb_random = 50
@@ -25,7 +20,7 @@ classifier_gender = tree.DecisionTreeClassifier()
 classifier_gender = classifier_gender.fit(X ,Y)
 
 new_element = [rnd.randint(171 ,181) , rnd.randint(70 , 90),rnd.randint(43 , 46)]
-     
+
 #method to choose and predict as many measurements that we want            
 def gender_prediction(number_element_predict):
     
@@ -39,27 +34,36 @@ def gender_prediction(number_element_predict):
             predict_gender = classifier_gender.predict([new_element])
             #prediction of the gender new element
             predict_gender = list(predict_gender)
-            list_gender_predicted.append([element + 1 ,predict_gender , new_element])
+            list_gender_predicted.append(predict_gender + new_element)
         else : 
             
             new_element = [rnd.randint(155 ,170) , rnd.randint(50 , 65),rnd.randint(39 , 43)]
             predict_gender = classifier_gender.predict([new_element])
             predict_gender = list(predict_gender)
-            list_gender_predicted.append([element + 1 , predict_gender , new_element])
-
-    return(list_gender_predicted)
+            list_gender_predicted.append(predict_gender  +  new_element)
+            
     
-#screen printing of the gender prediction   
-print(gender_prediction(10))
+    gender_predicted = pd.DataFrame(list_gender_predicted , columns= ['gender' , 'height' , 'weight' , 'shoe size'])
 
-#results
-#[[1, ['female'], [157, 62, 43]],
-# [2, ['male'], [181, 87, 43]], 
-# [3, ['female'], [165, 56, 39]], 
-# [4, ['female'], [168, 57, 40]], 
-# [5, ['male'], [175, 85, 45]], 
-# [6, ['female'], [163, 61, 41]], 
-# [7, ['male'], [181, 89, 46]], 
-# [8, ['female'], [159, 51, 43]],
-# [9, ['male'], [174, 80, 45]], 
-# [10, ['female'], [162, 52, 43]]
+
+    return(gender_predicted)
+  
+  #exemple with 10
+  #screen printing of the gender prediction
+#print("How many people in total do you want to predict?")
+#number_of_gender_predict = int(input())
+#print("These are the randomly genearated people that you asked for:")
+#print(gender_prediction(number_of_gender_predict).head())
+
+#How many people in total do you want to predict?
+#10
+#These are the randomly genearated people that you asked for:
+#   gender  height  weight  shoe size
+#0    male     174      88         44
+#1  female     165      63         41
+#2    male     172      84         45
+#3    male     178      87         45
+#4  female     168      52         43
+
+
+  
